30cdbbc40841bd27c57e170d6e07b50c3a703a95
Theavy Vun <theavy.vun@axivit.com>
2024-06-03 12:06:47 +0700

    feat(backend): impl new endpoint

--- back/app.js
+++ back/app.js
@@ -6,6 +6,12 @@ const cors = require("cors");
 const userRoute = require("./routes/user-route");
 const imageRoute = require("./routes/image-route");
 const productRoute = require("./routes/product-route");
+const sizeRoute = require("./routes/size-route");
+const careRoute = require("./routes/care-route");
+const lightRoute = require("./routes/light-route");
+const plantTypeRoute = require("./routes/plant-type-route");
+const plantGiftRoute = require("./routes/plant-gift-route");
+const plantLifeStyleRoute = require("./routes/plant-life-style-route");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +28,12 @@ app.use("/image", express.static("image"));
 app.use("/user", userRoute);
 app.use("/image", imageRoute);
 app.use("/product", productRoute);
+app.use("/size", sizeRoute);
+app.use("/care", careRoute);
+app.use("/light", lightRoute);
+app.use("/plant-type", plantTypeRoute);
+app.use("/plant-gift", plantGiftRoute);
+app.use("/plant-life-style", plantLifeStyleRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
